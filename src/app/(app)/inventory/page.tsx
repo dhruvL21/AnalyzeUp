e8d049b901aa9706cfa08b8eea0a88e8a88d18d9
Dhruv Lukhi <lukhidhruv97@gmail.com>
2025-11-30 12:34:10 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/(app)/inventory/page.tsx
+++ src/app/(app)/inventory/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import Image from "next/image";
-import React, "use state";
+import React, { useState } from "react";
 import { PlusCircle, MoreHorizontal } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import {
